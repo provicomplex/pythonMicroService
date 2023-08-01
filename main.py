51032e5f5cdc27c12600b0b29f43eee9c9f76985
provicomplex <tu_correo@ejemplo.com>
2023-08-01 17:24:35 -0300

    -Cambio dinamico en las imagenes, detecta desde cientos a miles -metodo para obtencion de fechas

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 from fastapi.responses import FileResponse
 from gtts import gTTS
 from fastapi import FastAPI, Body
+from matplotlib import ticker
 from pydantic import BaseModel
 from typing import List, Tuple
 import matplotlib.pyplot as plt
 from multiprocessing import set_start_method
 from multiprocessing import Process, Manager
+from dateparser.search import search_dates
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -21,6 +23,8 @@ class Item(BaseModel):
     mes: str
     monto: float
 
+class TextoModel(BaseModel):
+    texto: str
 
 
 @app.get("/")
@@ -38,11 +42,16 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
+
+
 @app.post("/text/")
 async def texToAudio(data: dict = Body(..., embed=True)):
     # Obtener el texto y el idioma del JSON recibido
     texto_recibido = data.get("texto", "")
-    idioma_recibido:str= data.get("idioma", "")
+    idioma_recibido: str = data.get("idioma", "")
+
+    # Agregar comillas triples al texto recibido
+    texto_recibido = f'\'\'\'{texto_recibido}\'\'\''
 
     # Directorio donde se guardarán los archivos de audio
     directorio_uploads = "uploads"
@@ -60,8 +69,9 @@ async def texToAudio(data: dict = Body(..., embed=True)):
 
 
 
+
 @app.post("/datos/")
-async def recibir_datos(datos: List[Item]):
+def recibir_datos(datos: List[Item]):
     # Extraer los datos de mes y monto para el gráfico
     meses = [item.mes for item in datos]
     montos = [item.monto for item in datos]
@@ -72,11 +82,62 @@ async def recibir_datos(datos: List[Item]):
     plt.ylabel("USD")
     plt.title("Monthly billing")
 
+    # Formatear el eje y para mostrar valores legibles automáticamente
+    def format_y_axis(value, _):
+        if value >= 1e9:
+            return f'{value / 1e9:.0f}B'
+        elif value >= 1e6:
+            return f'{value / 1e6:.0f}M'
+        elif value >= 1e3:
+            return f'{value / 1e3:.0f}K'
+        else:
+            return f'{value:.0f}'
+
+    plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(format_y_axis))
+
+    # Ajustar el espaciado entre los ticks del eje x (meses)
+    plt.xticks(range(len(meses)), meses, rotation=45, ha="right")
+
     # Guardar el gráfico en un archivo JPEG con alta calidad (calidad 95)
-    plt.savefig("grafico.jpg", format="jpeg", quality=95)
+    plt.savefig("grafico.jpg", format="jpeg")
 
     # Cerrar la figura para liberar memoria
     plt.close()
 
     # Devolver la imagen generada como respuesta
     return FileResponse("grafico.jpg", media_type="image/jpeg")
+
+
+# async def recibir_datos(datos: List[Item]):
+#     # Extraer los datos de mes y monto para el gráfico
+#     meses = [item.mes for item in datos]
+#     montos = [item.monto for item in datos]
+
+#     # Crear el gráfico de barras
+#     plt.bar(meses, montos)
+#     plt.xlabel("Month")
+#     plt.ylabel("USD")
+#     plt.title("Monthly billing")
+
+#     # Guardar el gráfico en un archivo JPEG con alta calidad (calidad 95)
+#     plt.savefig("grafico.jpg", format="jpeg", quality=95)
+
+#     # Cerrar la figura para liberar memoria
+#     plt.close()
+
+#     # Devolver la imagen generada como respuesta
+#     return FileResponse("grafico.jpg", media_type="image/jpeg")
+
+@app.post("/stringToDate/")
+async def string_to_date(texto_model: TextoModel):
+    texto = texto_model.texto
+    extracted_dates = []
+    dates = search_dates(texto)
+    if dates is not None:
+        for d in dates:
+            extracted_dates.append(str(d[1]))
+    else:
+        extracted_dates.append('None')
+
+    print(extracted_dates)
+    return extracted_dates
